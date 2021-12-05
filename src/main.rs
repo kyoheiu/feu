06d87a1f3bac51ab51d92fd672a8c4d0f9505c5c
Kyohei Uto <kyoheiu@outlook.com>
2021-12-05 14:37:30 +0900

    Add subscription

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use iced::{text_input, Column, Element, Sandbox, Settings, Text, TextInput};
+use iced::scrollable::{self, Scrollable};
+use iced::{
+    keyboard, text_input, Application, Column, Container, Element, Settings, Subscription, Text,
+    TextInput,
+};
+use iced_native::{event, subscription, Event};
 
 struct Lists {
+    scroll: scrollable::State,
     input: text_input::State,
     input_value: String,
     cursor: usize,
@@ -32,6 +38,7 @@ impl Default for Lists {
         bin_vec.sort();
 
         Lists {
+            scroll: scrollable::State::new(),
             input: text_input::State::focused(),
             input_value: "".to_string(),
             cursor: 1,
@@ -41,11 +48,13 @@ impl Default for Lists {
     }
 }
 
-impl Sandbox for Lists {
+impl Application for Lists {
+    type Executor = iced::executor::Default;
     type Message = Message;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self::default()
+    fn new(_flags: ()) -> (Self, iced::Command<Message>) {
+        (Self::default(), iced::Command::none())
     }
 
     fn title(&self) -> String {
@@ -53,24 +62,22 @@ impl Sandbox for Lists {
     }
 
     fn view(&mut self) -> Element<Message> {
-        let input = TextInput::new(
-            &mut self.input,
-            "Run!",
-            &self.input_value,
-            Message::InputChanged,
-        );
-
         let bins_list: Element<Message> = {
             self.filtered
                 .iter()
-                .take(10)
-                .fold(Column::new(), |column, item| {
-                    column.push(Element::new(Text::new(item)))
+                .take(5)
+                .enumerate()
+                .fold(Scrollable::new(&mut self.scroll), |column, (i, item)| {
+                    if i + 1 == self.cursor {
+                        column.push(Element::new(Text::new(item).color([0.5, 0.5, 0.5])))
+                    } else {
+                        column.push(Element::new(Text::new(item)))
+                    }
                 })
                 .into()
         };
 
-        Column::new()
+        let content = Column::new()
             .padding(20)
             .push(TextInput::new(
                 &mut self.input,
@@ -78,11 +85,18 @@ impl Sandbox for Lists {
                 &self.input_value,
                 Message::InputChanged,
             ))
-            .push(bins_list)
-            .into()
+            .push(bins_list);
+
+        Container::new(content).into()
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(
+        &mut self,
+        message: Message,
+        _clipboard: &mut iced::Clipboard,
+    ) -> iced::Command<Message> {
+        let len = self.bins.len();
+
         match message {
             Message::InputChanged(words) => {
                 self.input_value = words;
@@ -93,9 +107,48 @@ impl Sandbox for Lists {
                     .map(|item| item.to_string())
                     .collect();
             }
-            Message::MoveCursor(mv) => {}
+            Message::MoveCursor(mv) => match mv {
+                Move::Up => {
+                    if self.cursor > 1 {
+                        self.cursor -= 1;
+                    } else {
+                    }
+                }
+                Move::Down => {
+                    if self.cursor < len {
+                        self.cursor += 1;
+                    } else {
+                    }
+                }
+                _ => {}
+            },
             Message::Execute => {}
         }
+        iced::Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        subscription::events_with(|event, status| {
+            if let event::Status::Captured = status {
+                return None;
+            }
+
+            match event {
+                Event::Keyboard(keyboard::Event::KeyPressed {
+                    modifiers,
+                    key_code,
+                }) => handle_key(key_code),
+                _ => None,
+            }
+        })
+    }
+}
+
+fn handle_key(key_code: keyboard::KeyCode) -> Option<Message> {
+    match key_code {
+        keyboard::KeyCode::Up => Some(Message::MoveCursor(Move::Up)),
+        keyboard::KeyCode::Down => Some(Message::MoveCursor(Move::Down)),
+        _ => None,
     }
 }
 
