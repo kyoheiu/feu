fc47e0d14cfb2d9949c7a53c0cbd89ad7ea3a0d8
kyoheiu <kyoheiu@outlook.com>
2022-09-18 06:50:00 +0900

    Impl From for Error, instead of thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,20 @@
-use thiserror::Error;
+#[derive(Debug)]
+pub struct FeuError(String);
 
-#[derive(Error, Debug)]
-pub enum FeuError {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error(transparent)]
-    Miniserde(#[from] miniserde::Error),
+impl From<std::io::Error> for FeuError {
+    fn from(err: std::io::Error) -> Self {
+        FeuError(err.kind().to_string())
+    }
+}
+
+impl From<std::env::VarError> for FeuError {
+    fn from(err: std::env::VarError) -> Self {
+        FeuError(err.to_string())
+    }
+}
+
+impl From<miniserde::Error> for FeuError {
+    fn from(_err: miniserde::Error) -> Self {
+        FeuError("Cannot read config json.".to_string())
+    }
 }
